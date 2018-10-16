5b06aa5e9d3de9a88c954b5d0d9c171fff0131c6
YOUR NAME <YOUR EMAIL ADRESS>
2018-10-16 11:44:59 +0200

    Les aliens peuvent se déplacer

--- scripts/aliens.js
+++ scripts/aliens.js
@@ -2,6 +2,10 @@ const NB_ALIENS_PER_LINE = 11;
 
 ALIEN_SPACE_X = 35;
 ALIEN_SPACE_Y = 28;
+aliensTimer = 500;
+
+
+let lastAlienMovement = 0; // instant t du dernier déplacement déplacement des aliens
 
 const aliensMap = [
     '40','40','40','40','40','40','40','40','40','40','40',
@@ -11,7 +15,7 @@ const aliensMap = [
     '10','10','10','10','10','10','10','10','10','10','10',
 ];
 
-const alienSprites = {
+const alienSprites = { // Mettre aliens avec appuie et F2
     40 : [
             {x : 6, y : 3, width : 16, height : 16 },
             {x : 6, y : 25, width : 16, height : 16}
@@ -39,11 +43,12 @@ function createAliens(){
         }
 
         aliens.push({
-            x : 10 + i % NB_ALIENS_PER_LINE * ALIEN_SPACE_X, // Gérer espacement chaque aliens
+            x : 12 + i % NB_ALIENS_PER_LINE * ALIEN_SPACE_X, // Gérer espacement chaque aliens
             y : 100 + line *  ALIEN_SPACE_Y, 
             width : alienWidth,
             height : alienHeight,
             points : aliensMap[i],
+            direction : 1,
             spriteIndex : 0
             // id : aliensMap[i] // A, B ou C
         });
@@ -52,8 +57,34 @@ function createAliens(){
 }
 
 function animateAliens(){
+    // Parcours du tableau d'aliens pour mise à jour 
+    
+    // Mouvement des aliens de gauche à droite et vers le bas
+    if(Date.now() - lastAlienMovement > aliensTimer){
+        lastAlienMovement = Date.now(); // Mise à jour de l'instant du dernier mouvement du joueur à maintenant 
 
-}
+        // Récupération du X de l'alien le plus à droite (et à gauche)
+        let extremeRightAlien = Math.max( ... aliens.map(a => a.x) ) + ALIEN_SPACE_X;
+        let extremeLeftAlien = Math.min( ... aliens.map(a => a.x) );
+
+        // Parcours du tableau des aliens pour mise à jour
+        for (let i = 0; i < aliens.length; i ++){
+
+            if(
+                extremeRightAlien > canvas.width && aliens[i].direction === 1 ||
+                extremeLeftAlien <= 0 && aliens[i].direction === -1
+            ) {
+                aliens[i].direction *= -1;
+                aliens[i].y += 22;
+            }
+            else{
+                aliens[i].x += 12 * aliens[i].direction;
+            }
+
+        aliens[i].x += 12 * aliens[i].direction;
+        }
+    }
+} // Fin du mouvement des aliens 
 
 function renderAliens(){
     for(let i = 0; i < aliens.length; i++){
