b4aff51d1ae28ed6a75dd09a6c216da03e43dcfa
YOUR NAME <YOUR EMAIL ADRESS>
2018-10-19 10:31:44 +0200

    Game over and press f5 create

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const context = canvas.getContext('2d');
 canvas.width = 480;
 canvas.height = 540;
 
-let timer;
+let timer = 0;
 let player;
 let aliens;
 const sounds = {
@@ -18,6 +18,10 @@ const sounds = {
     shoot : document.getElementById('shoot'),
     player_death : document.getElementById('player_death'),
 }
+
+const MODE_PLAYING = 1;
+const MODE_GAME_OVER = 2;
+let game_mode = MODE_GAME_OVER ;
 
 // Chargement de l'image du sprite avant de démarrer le jeu
 
@@ -33,16 +37,27 @@ spritesheet.onload = function(){ // Fonction éxécutée lorsque le navigateur a
 
 // Fonction pour les actions
 function update() {
-    animatePlayer(); //Fonction qui gèrera l'animation du joueur
-    animateAliens(); // Fonction alien
+    switch(game_mode){
+        case MODE_PLAYING :
+            animatePlayer(); //Fonction qui gère l'animation du joueur
+            animateAliens(); // Fonction qui gère l'animation de l'alien
+            break;
+        }
 }
 
 // Fonction pour les dessins
 function render() {
     context.clearRect(0, 0, canvas.width, canvas.height); // On efface 60/s et on re dessine
 
-    renderPlayer(); //Fonction qui gèrera le dessin du joueur
-    renderAliens(); // Dessin de l'alien
+    switch(game_mode){
+        case MODE_PLAYING :
+            renderPlayer(); // Fonction qui gèrera le dessin du joueur
+            renderAliens(); // Fonction qui gère le dessin de l'alien
+            break;
+        case MODE_GAME_OVER : 
+            renderGameOver(); // Fonction qui gère le game over
+            break;
+        }
     renderUI(); // Dessin éléments de l'interface 
 }
 
@@ -54,5 +69,27 @@ function gameloop() {
     timer = window.requestAnimationFrame(gameloop); // Permet d'éxecuter la fonction à chaque fois 
 }
 
+let go_color = 0;
+let go_size = 0;
 
+// Fonction gérant le mode game over
+function renderGameOver(){
 
+    go_color += 20;
+    go_size = 24 - (timer % 4);
+
+    if(game_mode === MODE_GAME_OVER){
+
+        context.textAlign = "center"; // Alligner
+        context.fillStyle = "hsl(" +go_color+ " , 100% ,50%)"; // Couleur clignotter
+        context.font = 'normal' +go_size+ "Arial"; // Police
+        context.fillText('GAME OVER', canvas.width/2, canvas.height/2); // Texte
+
+        context.fillStyle = '#fff';
+        context.font="18px Arial";
+        context.fillText("Press F5 ", canvas.width/2, canvas.height/2+30)
+
+    }else if(game_mode === MODE_PLAYING){
+        render();
+    }
+}
